37a214a885cd135e3172108c276b8128378d1e1a
StanleyXie <stanley.xie@outlook.com>
2024-10-29 17:19:43 +0100

    Update inputs

--- connectivity/main.tf
+++ connectivity/main.tf
@@ -34,8 +34,6 @@ provider "azurerm" {
   # client_id       = var.client_id
   # client_secret   = var.client_secret
   # tenant_id       = var.tenant_id
-  client_id     = var.client_id
-  client_secret = var.client_secret
 }
 
 # Get the current client configuration from the AzureRM provider
